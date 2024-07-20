ad8d192489636b97d6b43f4f1c2ca1d0fbffd5a1
Shekeeb <shekeebmohammed10@gmailcom>
2024-07-20 18:37:13 +0530

    Hosting

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,41 +1,41 @@
 import userModel from "../models/userModel.js";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
-import validator from "validator"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
 
 //login user
 const loginUser = async (req, res) => {
-    const {email,password}=req.body;
+    const { email, password } = req.body;
     try {
-        const user=await userModel.findOne({email});
+        const user = await userModel.findOne({ email });
         if (!user) {
             return res.json({
-                success:false,
-                message:"User doesn't exist..!"
-            })
+                success: false,
+                message: "User doesn't exist..!"
+            });
         }
 
-        const isMatch=await bcrypt.compare(password,user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
             return res.json({
-                success:false,
-                message:"Invalid credentials..!"
-            })
+                success: false,
+                message: "Invalid credentials..!"
+            });
         }
 
-        const token=createToken(user._id);
+        const token = createToken(user._id);
         res.json({
-            success:true,
+            success: true,
             token
-        })
+        });
 
     } catch (error) {
         console.log(error);
         res.json({
-            success:false,
-            message:"Error..!"
-        })
+            success: false,
+            message: "Error..!"
+        });
     }
 }
 
@@ -53,7 +53,7 @@ const registerUser = async (req, res) => {
             return res.json({
                 success: false,
                 message: "User already exist..!"
-            })
+            });
         }
 
         //validating email 
@@ -61,7 +61,7 @@ const registerUser = async (req, res) => {
             return res.json({
                 success: false,
                 message: "Please enter a valid email..!"
-            })
+            });
         }
 
         //validating password
@@ -69,7 +69,7 @@ const registerUser = async (req, res) => {
             return res.json({
                 success: false,
                 message: "Password must contain 8 characters..!"
-            })
+            });
         }
 
         //hashing password
@@ -80,21 +80,21 @@ const registerUser = async (req, res) => {
             name: name,
             email: email,
             password: hashedPassword
-        })
+        });
 
         const user = await newUser.save();
-        const token=createToken(user._id);
+        const token = createToken(user._id);
         res.json({
-            success:true,
+            success: true,
             token
-        })
+        });
 
     } catch (error) {
         console.log(error);
         res.json({
-            success:false,
-            message:"Error..!"
-        })
+            success: false,
+            message: "Error..!"
+        });
     }
 }
 
